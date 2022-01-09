81b42b9b74ed61771516048350de522d78e40316
Kyle June <github@kylejune.com>
2022-01-09 10:31:13 -0600

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-export { delay } from "https://deno.land/std@0.114.0/async/delay.ts";
+export { delay } from "https://deno.land/std@0.120.0/async/delay.ts";
 export {
   assert,
   assertEquals,
   AssertionError,
   assertObjectMatch,
+  assertRejects,
   assertThrows,
-  assertThrowsAsync,
-} from "https://deno.land/std@0.114.0/testing/asserts.ts";
+} from "https://deno.land/std@0.120.0/testing/asserts.ts";
 
 export { Vector } from "https://deno.land/x/collections@0.11.2/vector.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -1,6 +1,6 @@
-export { spy, stub } from "https://deno.land/x/mock@0.12.0/mod.ts";
+export { spy, stub } from "https://deno.land/x/mock@0.12.2/mod.ts";
 export type {
   Spy,
   SpyCall,
   Stub,
-} from "https://deno.land/x/mock@0.12.0/mod.ts";
+} from "https://deno.land/x/mock@0.12.2/mod.ts";
